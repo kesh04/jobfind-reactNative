fc301276b8acfe6704eccca13b4ff4fdb01fea07
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2023-10-15 17:50:54 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,51 @@
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { View, Text,  ScrollView,SafeAreaView} from "react-native";
+ import { COLORS ,icons, images, SIZES } from "../constants";
+import { Nearbyjobs, ScreenHeaderBtn,  Welcome, Popularjobs} from '../components'
+
+ const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+    const router = useRouter();
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor:COLORS.lightWhite}}>
+    <Stack.Screen  
+    options={{
+      headerStyle:{backgroundColor:COLORS.lightWhite},
+      headerShadowVisible:false,
+      headerLeft:()=>(
+        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+      ),
+      headerRight:()=>(
+        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+      ),
+      headerTitle:""
+    }}/>
+
+    <ScrollView showsVerticalScrollIndicator={false}>
+       <View style={{
+        flex:1,
+        padding: SIZES.medium
+       }}>
+        <Welcome
+           searchTerm={searchTerm}
+           setSearchTerm={setSearchTerm}
+           handleClick={() => {
+             if (searchTerm) {
+               router.push(`/search/${searchTerm}`)
+             }
+           }}
+        />
+
+           <Popularjobs />
+        <Nearbyjobs 
+       />
+  
+
+       </View>
+    </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
